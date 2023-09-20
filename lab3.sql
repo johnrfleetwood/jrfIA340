b81b77e360ad750df816d14b022876b4c8b37c04
johnrfleetwood <122939195+johnrfleetwood@users.noreply.github.com>
2023-09-20 16:00:33 -0400

    lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -64,3 +64,10 @@ inner join course
 on professor.p_email = course.p_email
 
 --2.10
+select p_name, count(*) as count_course
+from professor
+inner join course
+on professor.p_email = course.p_email
+group by p_name
+order by count_course desc
+limit 1;
